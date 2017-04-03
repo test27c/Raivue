fc5c0ed62f474393d9147306ea653026fe7451a6
ShuXian <test27c@gmail.com>
2017-04-03 15:03:45 +0700

    Add CRUD ability to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def new
     @article = Article.new
   end
@@ -13,8 +14,33 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article updated"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:success] = "Article deleted"
+    redirect_to articles_path
   end
 
   private
