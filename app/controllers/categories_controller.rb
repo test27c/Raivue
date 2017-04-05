0905176d102c9954d5a61076e255772e57697a29
ShuXian <test27c@gmail.com>
2017-04-05 13:05:04 +0700

    Fix categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,25 @@
 class CategoriesController < ApplicationController
-
+  
   before_action :require_admin, except: [:index, :show]
-
+  
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
+  
   def new
     @category = Category.new
   end
-
+  
+  def create
+    @category =  Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category was created successfully"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+  
   def edit
     @category = Category.find(params[:id])
   end
@@ -19,27 +33,25 @@ class CategoriesController < ApplicationController
       render 'edit'
     end
   end
-
-  def index
-    @categories = Category.paginate(page: params[:page], per_page: 5)  
-  end
-
+  
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
-
+  
   private
-
-    def category_params
-      params.require(:category).permit(:name)
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end
+  
+  private
+  
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "Only admin can perform that action"
+      redirect_to categories_path
     end
-
-    def require_admin
-      if !logged_in? || (logged_in? and !current_user.admin?)
-        flash[:danger] = "Only admin can perform that action"
-        redirect_to categories_path
-      end
-    end
+  end
   
 end
